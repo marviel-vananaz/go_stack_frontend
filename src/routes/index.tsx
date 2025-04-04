a4d236c8d36e8529582b8b6b41bf46a05eb65f20
marviel-vananaz <marviel@vananaz.com>
2025-04-04 08:51:36 +0800

    Basic fetching

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,32 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { Button } from '@/components/ui/button'
+import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
+import createClient from "openapi-fetch";
+import type { paths } from "@/lib/api/petstore";
+import { useMemo } from "react";
+import { useQuery } from "@tanstack/react-query";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: App,
-})
+});
 
 function App() {
+  let client = useMemo(
+    () => createClient<paths>({ baseUrl: "http://localhost:8080/" }),
+    [],
+  );
+
+  const query = useQuery({
+    queryKey: ["pets"],
+    queryFn: () => client.GET("/pet"),
+  });
   return (
     <div className="text-center">
-        <Button>Login</Button>
+      <Button>Login</Button>
+      {query.isSuccess ? (
+        <span>{query.data.data?.map((data) => data.name).join(", ")}</span>
+      ) : (
+        <span>Error!</span>
+      )}
     </div>
-  )
+  );
 }
